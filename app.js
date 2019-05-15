5ad3335dce35a7999fbdca2fd5f86fe6edd1500d
feetbo90 <feetbonike90@gmail.com>
2019-05-15 21:16:16 +0700

    blocking and non blocking code

--- app.js
+++ app.js
@@ -8,23 +8,35 @@ function rqListener(req, res) {
 
 }
 const server = http.createServer((req, res) => {
-    console.log(req);
+    //console.log(req);
     const url = req.url;
     const method = req.method;
-    if(url === '/') {
+    if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
 
     if (url === '/message' && method === 'POST') {
-        fs.writeFileSync('hello.txt',"Hello Coy");
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split("=")[1];
+            fs.writeFile('hello.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+
+            });
+        });
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
@@ -34,4 +46,4 @@ const server = http.createServer((req, res) => {
     res.end();
 });
 
-server.listen(3001);
+server.listen(3002);
