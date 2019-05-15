40f455ab0d7a765c8962d584275daba6e5c22527
feetbo90 <feetbonike90@gmail.com>
2019-05-15 17:07:27 +0700

    redirecting request

--- app.js
+++ app.js
@@ -3,19 +3,27 @@
  */
 
 const http = require('http');
-
+const fs = require('fs');
 function rqListener(req, res) {
 
 }
 const server = http.createServer((req, res) => {
     console.log(req);
     const url = req.url;
+    const method = req.method;
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="post" name="message"><input type="text"><button type="submit">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST" name="message"><input type="text"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('hello.txt',"Hello Coy");
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
