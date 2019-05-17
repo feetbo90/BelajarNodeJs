de430ea3910b8216465b9d6952a629575dfcaa0f
feetbo90 <feetbonike90@gmail.com>
2019-05-17 14:50:07 +0700

    middleware simple

--- app.js
+++ app.js
@@ -4,11 +4,28 @@
 
 const http = require('http');
 
-const routes = require('./routes')
+const express = require('express');
+//const routes = require('./routes')
 
-function rqListener(req, res) {
+function rqListener(req, res) {}
 
-}
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log("In the middleware");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("In the another middleware");
+    res.send('<h1>Hello from Express</h1>');
+});
+
+
+const server = http.createServer(app);
+
+//const server = http.createServer(routes.handler);
+
+//server.listen(3000);
+
+app.listen(3000);
