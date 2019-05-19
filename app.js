4519a1612df4129c3d7cb1e914e46a72f503eecf
feetbo90 <feetbonike90@gmail.com>
2019-05-19 14:52:17 +0700

    method post in body parser

--- app.js
+++ app.js
@@ -6,17 +6,31 @@ const http = require('http');
 
 const express = require('express');
 //const routes = require('./routes')
+const bodyParser = require('body-parser');
 
 function rqListener(req, res) {}
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/',(req, res, next) => {
+    console.log("this is always run");
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
+    console.log("In the middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Button</button></form><h1>Hello add product</h1>');
+    //next();
+});
+
+app.post('/product',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
     console.log("In the another middleware");
     res.send('<h1>Hello from Express</h1>');
 });
